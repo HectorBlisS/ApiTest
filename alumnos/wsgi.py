34eb8862a624fb19bb37e28715bd65342d96d6f5
Héctor Feliciano Campos Muñoz <BlisS@MacBook-Pro-de-Hector.local>
2016-09-23 19:00:00 -0500

    quité whitenoise

--- alumnos/wsgi.py
+++ alumnos/wsgi.py
@@ -6,6 +6,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alumnos.settings")
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
 
