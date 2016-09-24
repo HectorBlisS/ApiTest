aa57731f182aa070f1458d8fb18242f4499cdd79
Héctor Feliciano Campos Muñoz <BlisS@MacBook-Pro-de-Hector.local>
2016-09-23 20:37:55 -0500

    post

--- listado/api/views.py
+++ listado/api/views.py
@@ -2,6 +2,6 @@ from rest_framework import generics
 from ..models import Alumno
 from .serializers import AlumnoSerializer
 
-class Listado(generics.ListAPIView):
+class Listado(generics.ListCreateAPIView):
 	queryset = Alumno.objects.all()
 	serializer_class = AlumnoSerializer
