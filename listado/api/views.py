e05f63f284e0ba648b051554c00fa829b2b7eac3
Héctor Feliciano Campos Muñoz <BlisS@MacBook-Pro-de-Hector.local>
2016-10-04 10:49:17 -0500

    seguridad agregada a la vista

--- listado/api/views.py
+++ listado/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
 from ..models import Alumno
 from .serializers import AlumnoSerializer
+from rest_framework import authentication, permissions
 
 class Listado(generics.ListCreateAPIView):
 	queryset = Alumno.objects.all()
 	serializer_class = AlumnoSerializer
+	authentication_classes = (authentication.TokenAuthentication,)
